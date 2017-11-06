febd3cfe4253471ae642056a947e76d1a01fa36f
Piotr Bielak <piotrbielak2@gmail.com>
2017-11-06 19:17:10 +0100

    New version of memcached scaler

--- scaler/app.py
+++ scaler/app.py
@@ -1,65 +1,94 @@
-import json
 from math import ceil
 import os
-from shlex import split
-from subprocess import call
 from time import sleep
 
+from kubernetes import client, config
 import requests
 
-SCALING_THRESHOLD = os.environ.get('SCALING_THRESHOLD', 2)
-MAX_REPLICAS = os.environ.get('MAX_REPLICAS', 5)
-METRICS_HOST = os.environ.get('METRICS_HOST', 'memcached-metrics-service')
-METRICS_PORT = int(os.environ.get('METRICS_PORT', 5000))
+LATENCY_SCALING_THRESHOLD = int(os.environ.get('LATENCY_SCALING_THRESHOLD', 50))
+QPS_SCALING_THRESHOLD = int(os.environ.get('QPS_SCALING_THRESHOLD', 10000))
+MAX_REPLICAS = int(os.environ.get('MAX_REPLICAS', 5))
 
-def set_auth_token():
-    TOKEN_FILE = '/var/run/secrets/kubernetes.io/serviceaccount/token'
-    with open(TOKEN_FILE, 'r') as tokenf:
-        os.environ['MY_TOKEN'] = tokenf.read()
 
-def get_metrics():
-    res = requests.get(
-            'http://{}:{}/metrics'.format(
-                METRICS_HOST,
-                METRICS_PORT
-            )
-        )
+class InfluxDBConnector(object):
+    def __init__(self, host, port, dbname):
+        self.host = host
+        self.port = port
+        self.dbname = dbname
 
-    if not res.ok:
-        raise RuntimeError('Couldn\'t get answer from metrics-service')
+    def _get_url(self, operation):
+        return 'http://{}:{}/{}?db={}'.format(self.host, self.port, operation, self.dbname)
 
-    return res.json()
+    def get(self, query):
+        res = requests.get(self._get_url('query') + '&q={}'.format(query))
+
+        if not res.ok:
+            raise RuntimeError('An error occurred while getting data from influx: {}'.format(res.text))
+
+        return res.json()
+
+    def post(self, data):
+        res = requests.post(url=self._get_url('write'), data=data)
+
+        if not res.ok:
+            raise RuntimeError('Could not post to influx: {}'.format(res.text))
+
+
+def get_influxdb_connector():
+    host = os.environ.get('INFLUXDB_HOST', 'influxdb-service')
+    port = int(os.environ.get('INFLUXDB_PORT', 8086))
+    dbname = os.environ.get('INFLUXDB_DB_NAME', 'metrics')
+
+    return InfluxDBConnector(host, port, dbname)
+
+
+def get_metrics(influxdb_connector):
+    latency_percentile_query = 'SELECT+percentile(%22latency%22%2C+99)+FROM+%22metrics%22+WHERE+time+%3E%3D+now()+-+1m'
+    qps_query = 'SELECT+%22qps%22+FROM+%22metrics%22+WHERE+time+%3E%3D+now()+-+1m+limit+1'
+
+    lat_res = influxdb_connector.get(latency_percentile_query)
+    qps_res = influxdb_connector.get(qps_query)
+
+    metrics = {
+        'qps': qps_res['results'][0]['series'][0]['values'][0][1],
+        'latency_99th_percentile': lat_res['results'][0]['series'][0]['values'][0][1],
+    }
+    return metrics
+
+
+def scale_pod(api, deployment_name, nb_replicas):
+    body = [{"op":"replace","path":"/spec/replicas","value": str(nb_replicas)}]
+
+    return api.patch_namespaced_deployment_scale(name=deployment_name,
+                                                 namespace='default',
+                                                 body=body)
+
 
 def get_target_replica_count(metric_value, threshold):
     return min(max(ceil(float(metric_value) / threshold), 1), MAX_REPLICAS)
 
-def scale_pod(nb_replicas):
-    print 'Scaling to:', nb_replicas
-    cmd = ('curl -X PATCH -sSk'
-          ' -d \'[{"op":"replace","path":"/spec/replicas","value":"' + str(nb_replicas) + '"}]\''
-          ' -H \"Authorization: Bearer ' + str(os.environ.get('MY_TOKEN')) + '\"'
-          ' -H \"Accept: application/json\"'
-          ' -H \"Content-Type: application/json-patch+json\"'
-          ' https://kubernetes:443/apis/extensions/v1beta1/namespaces/default/deployments/memcached-deployment/scale')
-    print 'CMD = ', cmd
-    call(split(cmd))
 
-if __name__ == "__main__":
-    set_auth_token()
+def main():
+    config.load_incluster_config()
+    api = client.ExtensionsV1beta1Api()
+    influxdb_connector = get_influxdb_connector()
 
     last_replica_count = 1
     while True:
-        metrics = get_metrics()
-        percentile_99th = metrics['99th_percentile']
+        metrics = get_metrics(influxdb_connector)
+        replica_count = max(get_target_replica_count(metrics['latency_99th_percentile'], LATENCY_SCALING_THRESHOLD),
+                            get_target_replica_count(metrics['qps'], QPS_SCALING_THRESHOLD))
 
-        replica_count = get_target_replica_count(percentile_99th,
-                                                 SCALING_THRESHOLD)
-
-        print 'Calculated replica_count', replica_count
+        print 'Calculated replica_count:', replica_count
 
         if replica_count != last_replica_count:
-            scale_pod(replica_count)
+            print 'Scaling pod to:', replica_count
+            scale_pod(api, 'memcached-deployment', replica_count)
+            influxdb_connector.post('config,replicas={} replicas={}'.format(replica_count, replica_count))
             last_replica_count = replica_count
 
         sleep(20)
 
+
+if __name__ == "__main__":
+    main()
